94a269ee66880a3f87fa186bba51d074c5d24226
Guilherme Lopes Antunes <guilhermelantunes123@gmail.com>
2026-05-15 15:37:29 -0300

    First Part Configure

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,6 +2,10 @@ import {drizzle} from 'drizzle-orm/neon-http'
 import {neon} from "@neondatabase/serverless"
 import * as schema from './schema'
 
+/*Conexão Neon */
 const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle(sql, {schema});
+/*Conexão Docker */
+const sqlDocker = process.env.DATABASE_URL!
+
+    export const db = drizzle(sql, {schema});
