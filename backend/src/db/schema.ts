d607a507d82e68701d22f39f594f22e002a2ae48
Guilherme Lopes Antunes <guilhermelantunes123@gmail.com>
2026-05-20 17:56:33 -0300

    feat: Define database schema for users, classrooms, schools, and professors

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,12 +1,14 @@
-import {
-    pgTable,
-    numeric,
-    text,
-    timestamp,
-    uuid,
-    pgEnum,
-    boolean
-} from "drizzle-orm/pg-core"
+import { pgEnum } from "drizzle-orm/pg-core";
+import { 
+    pgTable, 
+    text, 
+    timestamp, 
+    boolean, 
+    uuid, 
+    integer, 
+    doublePrecision, 
+    primaryKey 
+} from "drizzle-orm/pg-core";
 
 export const userRoleEnum = pgEnum("user_role", ["student", "professor"])
 
@@ -27,22 +29,55 @@ export const users = pgTable("users", {
     updatedAt: timestamp("updated_at").defaultNow().notNull()
 });
 
+export const schools = pgTable("schools", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: text("name").notNull(),
+    code: text("code").notNull().unique(), 
+    active: boolean("active").default(true).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+});
 
-export const students = pgTable("studets", {
-    id: uuid('student_id')
+export const classrooms = pgTable("classrooms", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    schoolId: uuid("school_id")
         .notNull()
-        .references(() => users.id, {onDelete: "cascade"}),
+        .references(() => schools.id, { onDelete: "cascade" }),
+        name: text("name").notNull(),
+        code: text("code").notNull().unique(),
+        active: boolean("active").default(true).notNull(),
+        createdAt: timestamp("created_at").defaultNow().notNull(),
+    });
+
+export const students = pgTable("students", {
+    id: uuid('student_id')
+        .notNull() 
+        .references(() => users.id, { onDelete: "cascade" }),
+    level: integer("level").default(1).notNull(),
+    currentXp: integer("current_xp").default(0).notNull(), 
+    totalXp: integer("total_xp").default(0).notNull(),
+    levelProgress: doublePrecision("level_progress").default(0).notNull(),
+    rank: integer("rank"),
+    ruby: integer("ruby").default(0).notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull()
-})
-
+}, (table) => {
+  
+    return {
+        pk: primaryKey({ columns: [table.id] }) 
+    };
+});
 export const professors = pgTable("professors", {
     id: uuid('professor_id')
         .notNull()
-        .references(() => users.id, {onDelete: "cascade"}),
+        .references(() => users.id, { onDelete: "cascade" }),
+    specialization: text("specialization"),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull()
-})
+}, (table) => {
+    return {
+        pk: primaryKey({ columns: [table.id] })
+    };
+});
 
 export type User = typeof users.$inferSelect
 export type NewUser = typeof users.$inferInsert
